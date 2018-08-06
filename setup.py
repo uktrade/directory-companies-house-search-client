ec2b8a57ec307625c43479a9cfddb670cd4e43b1
konradko <konrad@korzel.com>
2018-08-06 12:37:54 +0100

    Update sigauth, switch reqs method

--- setup.py
+++ setup.py
@@ -1,32 +1,12 @@
 """
 Export Directory CH client
 """
-import ast
-import pip.download
-from pip.req import parse_requirements
-import re
 from setuptools import setup, find_packages
-
-
-def get_version():
-    pattern = re.compile(r'__version__\s+=\s+(.*)')
-
-    with open('directory_ch_client/version.py', 'rb') as src:
-        return str(ast.literal_eval(
-            pattern.search(src.read().decode('utf-8')).group(1)
-        ))
-
-
-def get_requirements():
-    return [str(r.req) for r in list(parse_requirements(
-        'requirements.txt',
-        session=pip.download.PipSession()
-    ))]
 
 
 setup(
     name='directory_ch_client',
-    version=get_version(),
+    version='0.3.0',
     url='https://github.com/uktrade/directory-companies-house-search-client',
     license='MIT',
     author='Department for International Trade',
@@ -34,7 +14,13 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     long_description=open('README.md').read(),
     include_package_data=True,
-    install_requires=get_requirements(),
+    install_requires=[
+        'django>=1.9,<2.0a1',
+        'requests>=2.18.4,<3.0.0',
+        'monotonic>=1.2,<3.0',
+        'monotonic>=1.2,<3.0',
+        'sigauth>=4.0.1,<5.0.0'
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
